86015bb9c086a80e5c4fe7f552ca44c6def72138
sheelamrajesh1990 <54777353+sheelamrajesh1990@users.noreply.github.com>
2021-05-29 18:39:19 +0530

    Update SpringBootDockerJenkinsIntegrationApplication.java

--- src/main/java/com/springboot/docker/api/SpringBootDockerJenkinsIntegrationApplication.java
+++ src/main/java/com/springboot/docker/api/SpringBootDockerJenkinsIntegrationApplication.java
@@ -8,7 +8,7 @@ public class SpringBootDockerJenkinsIntegrationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDockerJenkinsIntegrationApplication.class, args);
-		System.out.println("Test");
+		
 	}
 
 }
