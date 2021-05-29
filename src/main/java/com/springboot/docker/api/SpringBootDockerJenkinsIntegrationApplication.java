fc0d2e7ecf98b83e51f60d4f4f4434a00a08e60a
Rajesh <sheelamrajesh0409@gmail.com>
2021-05-29 15:08:46 +0530

    first commit

---
+++ src/main/java/com/springboot/docker/api/SpringBootDockerJenkinsIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.docker.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerJenkinsIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerJenkinsIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/docker/api/SpringBootDockerJenkinsIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.docker.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerJenkinsIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
